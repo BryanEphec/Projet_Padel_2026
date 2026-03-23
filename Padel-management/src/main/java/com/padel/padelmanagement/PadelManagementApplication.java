0bf6f79d6e399e581272955d7d42e841f4649223
BryanEphec <psr11274@students.ephec.be>
2026-03-23 23:53:16 +0100

    Backend Spring Boot 100% opérationnel et connecté à MSSQL

---
+++ Padel-management/src/main/java/com/padel/padelmanagement/PadelManagementApplication.java
@@ -0,0 +1,13 @@
+package com.padel.padelmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadelManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PadelManagementApplication.class, args);
+    }
+
+}
