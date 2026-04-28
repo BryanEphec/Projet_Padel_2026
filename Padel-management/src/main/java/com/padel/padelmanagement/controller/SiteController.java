5204b3e656cceaa7f370b46028142869d574ed4e
BryanEphec <psr11274@students.ephec.be>
2026-04-29 00:12:29 +0200

    Update SiteController.java

--- Padel-management/src/main/java/com/padel/padelmanagement/controller/SiteController.java
+++ Padel-management/src/main/java/com/padel/padelmanagement/controller/SiteController.java
@@ -1,4 +1,10 @@
 package com.padel.padelmanagement.controller;
 
+import com.padel.padelmanagement.service.SiteService;
+import com.padel.padelmanagement.entity.Site;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 public class SiteController {
+    
 }
