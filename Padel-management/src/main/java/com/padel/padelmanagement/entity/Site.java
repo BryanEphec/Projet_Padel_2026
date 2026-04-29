cf261512837d621f8abedc30f93db50e64206cdf
BryanEphec <psr11274@students.ephec.be>
2026-04-29 12:25:27 +0200

    Update Site.java

--- Padel-management/src/main/java/com/padel/padelmanagement/entity/Site.java
+++ Padel-management/src/main/java/com/padel/padelmanagement/entity/Site.java
@@ -4,16 +4,23 @@ package com.padel.padelmanagement.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalTime;
+
 @Entity
 @Table(name = "Sites")
 @Data
 public class Site {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_Site")
     private Long id;
 
-    @Column(nullable = false, length = 100)
-    private String nom;
+    @Column(name = "Nom_Site",nullable = false, length = 100)
+    private String Nom_Site;
 
-    private String adresse;
+    @Column(name = "Heure_Ouverture")
+    private LocalTime Heure_Ouverture;
+
+    @Column(name = "Heure_Fermeture")
+    private LocalTime Heure_Fermeture;
 }
